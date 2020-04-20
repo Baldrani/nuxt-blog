a2bf633b3228a7773bc354112c3fd6961c82c484
Baldrani <mael.mayon@free.fr>
2020-04-20 10:27:44 +0200

    Update to be usable on netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import EventService from './services/EventService.js'
+
 export default {
     mode: 'universal',
     /*
@@ -68,5 +70,14 @@ export default {
          ** You can extend webpack config here
          */
         extend(config, ctx) {}
+    },
+    generate: {
+        routes: () => {
+            return EventService.getEvents().then((response) => {
+                return response.data.map((event) => {
+                    return '/blog/' + event.id
+                })
+            })
+        }
     }
 }
